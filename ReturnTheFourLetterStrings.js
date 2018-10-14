6bfb17075238357c9fd18ca088035bbcea231a35
YQS <41525397+TomStone76@users.noreply.github.com>
2018-10-13 23:23:08 -0400

    Commit

--- ReturnTheFourLetterStrings.js
+++ ReturnTheFourLetterStrings.js
@@ -1,5 +1,7 @@
 // Create a function that takes an array of strings. Return all words in the array that are exactly four letters.
 
 function isFourLetters(arr) {
-    
+    var x = arr;
+    var four = x.filter(words => words.length === 4);
+    return four;
 }
