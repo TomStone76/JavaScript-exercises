d3fa0eaf19595af4a1d2550d8b1292e82be582d7
YQS <41525397+TomStone76@users.noreply.github.com>
2018-10-14 10:32:31 -0400

    commit

---
+++ filterOutStrings.js
@@ -0,0 +1,8 @@
+// Create a function that takes an array of positive numbers and strings and returns a new array without the strings. 
+// In other words, remove all strings from an array of elements.
+// •Filter out all strings.
+// •Return only numbers.
+
+function filterArray(arr) {
+	
+}
