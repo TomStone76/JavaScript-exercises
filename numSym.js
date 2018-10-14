d022c8f2d2fb36fbadc34fe68fe249fc78320cb9
YQS <41525397+TomStone76@users.noreply.github.com>
2018-10-13 23:48:12 -0400

    commit

---
+++ numSym.js
@@ -0,0 +1,14 @@
+// Create a function that takes a number as an argument and returns true or false depending on whether the number is symmetrical or not.
+// A number is symmetrical when it is the same as its reverse.
+
+function isSymmetrical(num) {
+    var x = num.toString()
+    var sym = x.split("").reverse().join("");
+    if (x === sym) {
+        return true;
+    } else {
+        return false;
+    }
+}
+
+//Thanks to https://stackoverflow.com/questions/38188696/trying-to-find-if-a-number-is-a-palindrome-in-js for help with the syntax.
