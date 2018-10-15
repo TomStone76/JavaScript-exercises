8179bf06f828a2f56df1927cb2c06a5585a2b8d8
YQS <41525397+TomStone76@users.noreply.github.com>
2018-10-15 14:13:22 -0400

    commit

--- beanCounting.js
+++ beanCounting.js
@@ -4,3 +4,15 @@
 // Next, write a function called countChar that behaves like countBs, 
 // except it takes a second argument that indicates the character that is to be counted 
 // (rather than counting only uppercase “B” characters). Rewrite countBs to make use of this new function.
+
+function countBs(str) {
+    let counter = 0;
+    for (let i = 0; i < str.length; i++) {
+        if (str[i] === "B") {
+            counter++;
+        }
+    }
+    return counter;
+}
+
+console.log(countBs("BBC"));
