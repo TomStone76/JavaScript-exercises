6305232d089b92b4339fcf85e5c0b2223684f1e1
YQS <41525397+TomStone76@users.noreply.github.com>
2018-10-16 23:24:28 -0400

    Commit

--- DeleteExtraElements.js
+++ DeleteExtraElements.js
@@ -1,3 +1,10 @@
 // Create a function that takes two arguments: an array and a number. 
 // In the array (the first argument), if an element occures more than N times (the second argument), 
 // remove the extra occurences and return the result.
+
+function deleteOccurrences(arr, num) {
+    let x = arr;
+    for (let i = 0; i < x.length; i++) {
+        
+    }
+}
