d4507f3b12e47bc15ed532597803b58b2b2375ac
YQS <41525397+TomStone76@users.noreply.github.com>
2018-10-18 22:48:00 -0400

    commit

--- sortNumbers.js
+++ sortNumbers.js
@@ -4,3 +4,7 @@
 // Sort numbers array in ascending order.
 // If functions argument is null, an empty array or undefined, return an empty array.
 // Return new array of sorted numbers.
+
+function sortNumsAscending(arr) {
+    
+}
