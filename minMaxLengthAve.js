1f0313c4abd31edde6e706a9c639e870c48b3aff
YQS <41525397+TomStone76@users.noreply.github.com>
2018-10-23 11:52:40 -0400

    commit

---
+++ minMaxLengthAve.js
@@ -0,0 +1,9 @@
+// Create a function that takes an array of numbers and returns the following statistics:
+// Minimum Value
+// Maximum Value
+// Sequence Length
+// Average Value
+
+function minMaxLengthAverage(arr) {
+	
+}
