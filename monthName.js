7f3e6035688345fbbed095f9da19add52f9d5358
YQS <41525397+TomStone76@users.noreply.github.com>
2018-10-23 11:50:59 -0400

    commit

--- monthName.js
+++ monthName.js
@@ -2,5 +2,9 @@
 // return its corresponding month name as a string.
 
 function month_name(num) {
-	
+    var monthArr = ["", "January", "February", "March", "April", "May", 
+    "June", "July", "August", "September", "October", "November", "December"];
+    for (var i = 0; i < monthArr.length; i++) {
+        return(monthArr[num]);
+    }
 }
