dfbe6362691b83b321aa33f9486d68d768655bed
YQS <41525397+TomStone76@users.noreply.github.com>
2018-10-23 11:31:30 -0400

    commit

--- headBodyTail.js
+++ headBodyTail.js
@@ -6,5 +6,25 @@
 // "My head, body, and tail.", otherwise, return "Incomplete.".
 
 function verifySubstrs(mainStr, head, body, tail) {
-	
+    var testOne, testTwo, testThree
+    if (mainStr.startsWith(head)) {
+        testOne = true;
+    } else {
+        testOne = false;
+    }
+    if (mainStr.includes(body)) {
+        testTwo = true;
+    } else {
+        testTwo = false;
+    }
+    if (mainStr.endsWith(tail)) {
+        testThree = true;
+    } else {
+        testThree = false;
+    }
+    if (testOne && testTwo && testThree) {
+        return "My head, body, and tail."
+    } else {
+        return "Incomplete.";
+    }
 }
