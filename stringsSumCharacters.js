eda9d38cac6c40a7419e123c6731f904f752620e
YQS <41525397+TomStone76@users.noreply.github.com>
2018-10-23 11:35:58 -0400

    commit

--- stringsSumCharacters.js
+++ stringsSumCharacters.js
@@ -1,2 +1,11 @@
 // Create a function that takes two strings as arguments and return either 
 // true or false depending on whether the number of their charcters is equal or not.
+
+function comp(str1, str2) {
+    var x = str1.length, y = str2.length;
+    if (x === y) {
+        return true;
+    } else {
+        return false;
+    }
+}
