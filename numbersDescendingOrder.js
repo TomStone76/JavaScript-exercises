c8c3e68789d50eff65ba65022ed134828f0f442f
YQS <41525397+TomStone76@users.noreply.github.com>
2018-10-25 16:50:29 -0400

    commit

--- numbersDescendingOrder.js
+++ numbersDescendingOrder.js
@@ -1,3 +1,7 @@
 // Create a function that takes any nonnegative number as an argument 
 // and return it with it's digits in descending order. 
 // Descending order is when you sort from highest to lowest.
+
+function sortDecending(num) {
+	
+}
