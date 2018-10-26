37c56b50c52c140de686b50a41b0aab13495e46e
YQS <41525397+TomStone76@users.noreply.github.com>
2018-10-25 23:48:49 -0400

    commit

--- largestNumInArrays.js
+++ largestNumInArrays.js
@@ -2,5 +2,9 @@
 // Return a new (single) array with the largest numbers of each.
 
 function findLargestNums(arr) {
-	
+    var newArr = []
+	for (var i = 0; i < arr.length; i++) {
+        var max = Math.max(arr[i]);
+        newArr.push(max);
+    }
 }
