5b54fbf537c853a92339ac3dd6852c882b8a0de4
YQS <41525397+TomStone76@users.noreply.github.com>
2018-10-26 21:35:05 -0400

    commit

--- highAndLow.js
+++ highAndLow.js
@@ -1,6 +1,16 @@
 // Create a function that accepts a string of space separated numbers 
 // and returns the highest and lowest number (as a string).
 
+//"1 2 3"
+
 function highLow(str) {
-	
+    let arr =  str.split(" "), firstArr = [], secondArr = [];
+    for (let i = 0; i < arr.length; i++) {
+        let x = parseInt(arr[i])
+        firstArr.push(x);
+    }
+    let max = Math.max(...firstArr), min = Math.min(...firstArr);
+    secondArr.push(max, min)
+    return secondArr.join(" ")
 }
+
