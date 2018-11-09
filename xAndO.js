ba8f00e7c0ed088fee5de071f4044b4aebb62866
YQS <41525397+TomStone76@users.noreply.github.com>
2018-11-08 22:17:50 -0500

    commit

---
+++ xAndO.js
@@ -0,0 +1,6 @@
+// Create a function that takes a string, checks if it has the same number of 'x's and 'o's
+// and returns either true or false.
+
+// Return a boolean value (true or false).
+// The string can contain any character.
+// When neither an x nor an o is in the string, return true.
