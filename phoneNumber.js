bccf6b300b1b8254e027c4f8645cf8a29d5be4ac
YQS <41525397+TomStone76@users.noreply.github.com>
2018-11-14 00:31:47 -0500

    Commit

--- phoneNumber.js
+++ phoneNumber.js
@@ -1,2 +1,6 @@
 // Create a function that takes an array of 10 numbers (between 0 and 9) 
 // and returns a string of those numbers formatted as a phone number (e.g. (555) 555-5555).
+
+function formatPhoneNumber(numbers) {
+    
+}
