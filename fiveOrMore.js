78e7da0356db77de89734a443c035fd08ede146f
YQS <41525397+TomStone76@users.noreply.github.com>
2018-11-22 23:58:33 -0500

    commit

--- fiveOrMore.js
+++ fiveOrMore.js
@@ -2,3 +2,7 @@
 // and returns the same string, but with all five or more letter words reversed.
 // Strings passed in will consist of only letters and spaces. 
 // Spaces will be included only when more than one word is present.
+
+function reverse(str) {
+	
+}
