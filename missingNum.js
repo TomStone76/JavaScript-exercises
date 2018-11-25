450ea54542a87aea0533b4a7375e64ff8508f883
YQS <41525397+TomStone76@users.noreply.github.com>
2018-11-24 23:48:05 -0500

    commit

--- missingNum.js
+++ missingNum.js
@@ -2,8 +2,12 @@ Create a function that takes an array of numbers between 1 and 10 (excluding one
 and returns the missing number.
 
 function missingNums(arr) {
-    var sorted = arr.sort();
+    let sorted = arr.sort();
+    let missing;
     for (let i = 0; i < sorted.length; i++) {
-        
+        if (sorted[i] - sorted[i-1] != 1) {
+            missing = i;
+        }
     }
+    return missing;
 }
