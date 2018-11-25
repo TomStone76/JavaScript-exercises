e85c95dd4e3d994e99acd69ccd43c972950df09c
YQS <41525397+TomStone76@users.noreply.github.com>
2018-11-24 23:42:56 -0500

    commit

--- fiveOrMore.js
+++ fiveOrMore.js
@@ -6,5 +6,6 @@
 function reverse(str) {
 	for (let i = 0; i < str.length; i++) {
         //1. .split() to create an array
+        //2. use
     }
 }
--- missingNum.js
+++ missingNum.js
@@ -2,5 +2,8 @@ Create a function that takes an array of numbers between 1 and 10 (excluding one
 and returns the missing number.
 
 function missingNums(arr) {
-	
+    var sorted = arr.sort();
+    for (let i = 0; i < sorted.length; i++) {
+        
+    }
 }
