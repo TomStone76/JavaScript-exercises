01965c3f92615ed85b7df1f717dae6d7550df211
YQS <41525397+TomStone76@users.noreply.github.com>
2018-11-25 23:53:58 -0500

    commit

--- fiveOrMore.js
+++ fiveOrMore.js
@@ -5,6 +5,7 @@
 
 function reverse(str) {
     let spl = str.split() //makes string into array
+    let x // empty variable to hold word to be reversed
 	for (let i = 0; i < spl.length; i++) { //initiates loop which will iterate through array
         if (i.length >= 5) {
             i.reverse()//
