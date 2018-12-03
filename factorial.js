d08f76182ecfa17ca740ba529b3831a56c6df3e2
YQS <41525397+TomStone76@users.noreply.github.com>
2018-12-03 00:08:42 -0500

    commit

--- factorial.js
+++ factorial.js
@@ -11,4 +11,13 @@ function factorial(int) {
     }
 }
 
+console.log(factorial(1));
+console.log(factorial(2));
+console.log(factorial(3));
+console.log(factorial(4));
 console.log(factorial(5));
+console.log(factorial(6));
+console.log(factorial(7));
+console.log(factorial(8));
+console.log(factorial(9));
+console.log(factorial(10));
