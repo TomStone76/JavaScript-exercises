8b6dd614d14f128586c41958b5c8657d6cd80563
YQS <41525397+TomStone76@users.noreply.github.com>
2018-12-03 23:35:30 -0500

    commit

--- beanCounting.js
+++ beanCounting.js
@@ -27,8 +27,4 @@ function countChar(str, charName) {
 
 function countBs(str) {
     return countChar(str, "B");
-}
-
-console.log(countBs("BBC"));
-
-console.log(countChar("Lorem ipsum dolor sit amet, consectetur adipiscing elit. Integer in purus congue, commodo leo at, varius libero. Maecenas ornare nisi ex, vitae blandit ipsum aliquam at. Sed eleifend justo vel sem venenatis vulputate. Orci varius natoque penatibus et magnis dis parturient montes, nascetur ridiculus mus. Mauris congue molestie odio eu placerat. Suspendisse mi nisl, bibendum quis aliquet id, imperdiet quis lectus. Praesent non lectus consectetur, tristique dui sodales, tempor quam. Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed nisl urna, tristique sed lobortis hendrerit, pretium et massa. Cras et faucibus elit, eu placerat mauris. Pellentesque rhoncus turpis vitae sem rhoncus placerat. Proin eget tellus malesuada, gravida est sed, malesuada purus. Fusce nisl nisi, aliquet vel tellus sit amet, sagittis mattis justo.", "e"));
+} 
