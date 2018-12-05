d9a1d1f7ce8c4575c5534ab4bf277cfe5a5e6a25
YQS <41525397+TomStone76@users.noreply.github.com>
2018-12-04 23:15:04 -0500

    delete old logic

--- prime.js
+++ prime.js
@@ -2,14 +2,6 @@
 // A prime number is any positive integer that is evenly divisible by only two divisors:
 // 1 and itself. The first ten prime numbers are 2, 3, 5, 7, 11, 13, 17, 19, 23 and 29.
 
-// function isPrime(num) {
-//     if (num > 1 && num % num === 0 && num % 1 === 0 ) {
-//         return true;
-//     } else if (num % num != 0 || num % 1 != 0) {
-//         return false;
-//     }
-// }
-
 function isPrime(num) {
     var start = 2;
     while (start <= Math.sqrt(num)) {
