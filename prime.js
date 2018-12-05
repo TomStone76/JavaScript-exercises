413f40c41f4a6ce3b8f9bf508ccf9f4bd5d134e4
YQS <41525397+TomStone76@users.noreply.github.com>
2018-12-04 23:14:07 -0500

    commit

--- prime.js
+++ prime.js
@@ -2,10 +2,31 @@
 // A prime number is any positive integer that is evenly divisible by only two divisors:
 // 1 and itself. The first ten prime numbers are 2, 3, 5, 7, 11, 13, 17, 19, 23 and 29.
 
+// function isPrime(num) {
+//     if (num > 1 && num % num === 0 && num % 1 === 0 ) {
+//         return true;
+//     } else if (num % num != 0 || num % 1 != 0) {
+//         return false;
+//     }
+// }
+
 function isPrime(num) {
-    if (num > 1 && num % num === 0 && num % 1 === 0 ) {
-        return true;
-    } else if (num % num != 0 || num % 1 != 0) {
-        return false;
+    var start = 2;
+    while (start <= Math.sqrt(num)) {
+        if (num % start ++ < 1) {
+            return false
+        }
     }
+    return num > 1
 }
+
+console.log(isPrime(1)); //false
+console.log(isPrime(2)); //true
+console.log(isPrime(3)); //true
+console.log(isPrime(4)); //false
+console.log(isPrime(5)); //true
+console.log(isPrime(6)); //false
+console.log(isPrime(7)); //true
+console.log(isPrime(8)); //false
+console.log(isPrime(9)); //false
+console.log(isPrime(10)); //false
