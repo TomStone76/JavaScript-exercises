cb8b1ce3f339b6b632c21c956a949a34a2f0ca40
YQS <41525397+TomStone76@users.noreply.github.com>
2018-12-04 23:05:10 -0500

    add logic

--- prime.js
+++ prime.js
@@ -3,5 +3,9 @@
 // 1 and itself. The first ten prime numbers are 2, 3, 5, 7, 11, 13, 17, 19, 23 and 29.
 
 function isPrime(num) {
-
+    if (num > 1 && num % num === 0 && num % 1 === 0 ) {
+        return true;
+    } else if (num % num != 0 || num % 1 != 0) {
+        return false;
+    }
 }
