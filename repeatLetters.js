404f97de109ccb498bc519c7088ebf6377e9c40b
YQS <41525397+TomStone76@users.noreply.github.com>
2018-12-04 22:46:41 -0500

    commit

--- repeatLetters.js
+++ repeatLetters.js
@@ -1,6 +1,7 @@
 // Create a function that takes a string and returns a string in which each character is repeated once.
 
 function doubleChar(str) {
+    let newStr = '';
     for (let i = 0; i < str.length; i++) {
         
     }
