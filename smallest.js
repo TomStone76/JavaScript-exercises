be4e5ce09a21310e1b7ac66b8861282c2d52b122
YQS <41525397+TomStone76@users.noreply.github.com>
2018-12-23 23:58:21 -0500

    slice logic

--- smallest.js
+++ smallest.js
@@ -1,6 +1,14 @@
 // Create a function that takes an array of numbers
 // and returns the sum of the two lowest positive numbers.
 
+
+
+// let duplicates = sorted.filter(function(x, y) {
+//     return sorted.indexOf(x) >= y;
+// });
+
 function sumTwoSmallestNums(arr) {
-	
+	let newArr = arr.filter(function(x, y) {
+        return newArr.indexOf(x) >= y;
+    }
 }
