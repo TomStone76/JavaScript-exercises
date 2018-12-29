5ff593e51926dbe7829a143af426904e48f81b8e
YQS <41525397+TomStone76@users.noreply.github.com>
2018-12-28 23:26:27 -0500

    commit

--- prime.js
+++ prime.js
@@ -21,4 +21,4 @@ console.log(isPrime(6)); //false
 console.log(isPrime(7)); //true
 console.log(isPrime(8)); //false
 console.log(isPrime(9)); //false
-console.log(isPrime(10)); //false
+console.log(isPrime(10)); 
--- xAndO.js
+++ xAndO.js
@@ -26,3 +26,5 @@ function XO(str) {
         return true;
     }
 }
+
+console.log(XO("abcdefghijklmnopqrstuvwxyz"));
