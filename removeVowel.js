2df02b454737bf697b22e6b6a8601a51101c3e8f
YQS <41525397+TomStone76@users.noreply.github.com>
2018-12-28 23:31:18 -0500

    Commit

--- removeVowel.js
+++ removeVowel.js
@@ -5,3 +5,9 @@ function remove(str) {
     var x = str.replace(/[aeiou]/g, "");
     return x
 }
+
+const y = "abcdefghijklmnopqrstuvwxyz";
+
+console.log(remove(y));
+
+//logs bcdfghjklmnpqrstvwxyz
