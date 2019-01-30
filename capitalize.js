04fb32335c8fdc83362fd810a9ea747dcfc6fb08
YQS <41525397+TomStone76@users.noreply.github.com>
2019-01-29 20:41:04 -0500

    Commit

---
+++ capitalize.js
@@ -0,0 +1,13 @@
+// Create a function that takes a string as an argument and 
+// converts the first character of each word to uppercase. 
+// Return the newly formatted string.
+
+const x = "The small pup gnawed a hole in the sock."
+
+function makeTitle(str) {
+    var arr = str.split(' ')
+    const capital = arr.map(x => x[0].toUpperCase() + x.substring(1))
+    return capital
+}
+
+console.log(makeTitle(x))
