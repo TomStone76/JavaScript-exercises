a7efa3de029c9c593e7e4fd7fe5806bffdf2f9fa
YQS <41525397+TomStone76@users.noreply.github.com>
2019-01-31 00:44:09 -0500

    commit

--- reverseFive.js
+++ reverseFive.js
@@ -3,16 +3,14 @@
 // Strings passed in will consist of only letters and spaces. 
 // Spaces will be included only when more than one word is present.
 
-
-var x = 'Yoshi is a cute dog.'
-
 function reverse(str) {
     var arr = str.split(' ')
-    const newArr = arr.map(word => {
-        word.toUpperCase()
+    const newArr = arr.map(function(item) {
+        if (item.length >= 5) {
+            return item.split('').reverse().join('')
+        } else if (item.length < 5) {
+            return item
+        }
     })
-    return newArr
-
+    return newArr.join(' ')
 }
-
-console.log(reverse(x))
