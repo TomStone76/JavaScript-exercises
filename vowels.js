61de3d7dadc9486761588e8c764ae1f73e2fee88
YQS <41525397+TomStone76@users.noreply.github.com>
2019-02-01 02:18:38 -0500

    commit

--- vowels.js
+++ vowels.js
@@ -1,5 +1,12 @@
 // Create a function that takes a string and returns the number (count) of vowels contained within it.
 
-function countVowels() {
-    
-}
+
+function countVowels(str) {
+    let arr = str.split(''), vowels = []
+    for (let x = 0; x < arr.length; x++) {
+        if (arr[x] === 'a' || arr[x] === 'e' || arr[x] === 'i' || arr[x] === 'o' || arr[x] === 'u' ) {
+            vowels.push(arr[x])
+        }
+    }
+    return vowels.length
+} 
