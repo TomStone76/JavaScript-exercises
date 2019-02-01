18b6d3550d6e6524fac7a33877ff45b2b4af6df6
YQS <41525397+TomStone76@users.noreply.github.com>
2019-02-01 12:38:28 -0500

    commit

--- middle.js
+++ middle.js
@@ -3,5 +3,13 @@
 // If the word's length is even, return the middle two characters.
 
 function getMiddle(str) {
-    
+    let len = str.length
+    if (len % 2 === 0) {
+        let x = str.substring(0, len/2)
+        let y = str.substring((len/2), len)
+        return x.slice(-1) + y.charAt(0)
+    } else if (len % 2 !== 0) {
+        let x = str.substring(0, (len/2) + 1)
+        return x.split('').pop()
+    }
 }
