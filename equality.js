cd55d053caa2b32cf504358593f31caa38a25528
YQS <41525397+TomStone76@users.noreply.github.com>
2019-01-31 23:52:04 -0500

    Commit

--- equality.js
+++ equality.js
@@ -1,12 +1,17 @@
 // Write a function that takes three input values (a, b, c) and returns the number of equal values.
 
-// function equal(a, b, c) {
-//     let arr = [];
-//     arr.push(a, b, c);
-//     let equals = arr.filter(function(x, y) {
-//         return arr.indexOf(a) >= b;
-//     })
-//     return equals.length;
-// }
-
-// console.log(equal(1, 1, 1))
+function equal(a, b, c) {
+    let arr = []
+    let counter = 0
+    arr.push(a, b, c);
+    for (let i = 0; i < arr.length; i++) {
+        if (i[0] === i[1]) {
+            counter++
+        } else if (i[0] === i[2]) {
+            counter++
+        } else if (i[1] === i[2]) {
+            counter++
+        }
+    }
+    return counter
+}
