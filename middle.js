5a08d2b6dd8d398edc8340947203babefc40349b
YQS <41525397+TomStone76@users.noreply.github.com>
2019-02-01 11:32:46 -0500

    commit

---
+++ middle.js
@@ -0,0 +1,7 @@
+// Create a function that takes a string and returns the middle character(s). 
+// If the word's length is odd, return the middle character. 
+// If the word's length is even, return the middle two characters.
+
+function getMiddle(str) {
+    
+}
