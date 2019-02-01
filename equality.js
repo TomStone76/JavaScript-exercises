c5ae7f6f2d8d95f78041cea070d8ecde3b9dcc04
YQS <41525397+TomStone76@users.noreply.github.com>
2019-02-01 01:24:22 -0500

    commit

--- equality.js
+++ equality.js
@@ -2,19 +2,15 @@
 
 function equal(a, b, c) {
     let arr = [], counter = 0
-
     arr.push(a, b, c);
-
     for (let i = 0; i < arr.length; i++) {
-        if (i[0] === i[1]) {
-            counter++
-        } else if (i[0] === i[2]) {
-            counter++
-        } else if (i[1] === i[2]) {
-            counter++
+        if (arr[0] === arr[1] && arr[1] === arr[2] && arr[0] === arr[2]) {
+            counter = 3
+        } else if (arr[0] !== arr[1] && arr[1] !== arr[2] && arr[0] !== arr[2]) {
+            counter = 0
+        } else {
+            counter = 2
         }
     }
     return counter
 }
-
-console.log(equal(1, 2, 3))
