f54501f8e4f4a6961608097cbe24bba4c4ba8912
YQS <41525397+TomStone76@users.noreply.github.com>
2019-02-03 15:04:25 -0500

    commit

---
+++ indexCaps.js
@@ -0,0 +1,6 @@
+// Create a function that takes a single string as argument and 
+// returns an ordered list containing the indexes of all capital letters in the string.
+
+function indexOfCaps(str) {
+    
+}
