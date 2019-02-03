ac9a5ea5e69c5717b20425e4c4705c571fb2dce0
YQS <41525397+TomStone76@users.noreply.github.com>
2019-02-03 15:18:51 -0500

    commit

--- indexCaps.js
+++ indexCaps.js
@@ -1,6 +1,17 @@
 // Create a function that takes a single string as argument and 
 // returns an ordered list containing the indexes of all capital letters in the string.
 
+var x = 'eDaBiT'
+
 function indexOfCaps(str) {
-    
+    let arr = str.split('')
+    let newArr = []
+    for (let i = 0; i < arr.length; i++) {
+        if (arr[i] == arr[i].toUpperCase() && arr[i].toUpperCase() != arr[i].toLowerCase()) {
+            newArr.push(arr.indexOf(arr[i]))
+        }
+    }
+    return newArr
 }
+
+console.log(indexOfCaps(x))
