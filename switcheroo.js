6bd4ae2680b24c78438807a8431a8de9a71eeb67
YQS <41525397+TomStone76@users.noreply.github.com>
2019-02-04 15:25:53 -0500

    commit

---
+++ switcheroo.js
@@ -0,0 +1,10 @@
+// Create a function that takes a string and returns a new string with its first and last characters swapped, 
+// except under three conditions:
+
+// If the length of the string is less than two, return "Incompatible.".
+// If the argument is not a string, return "Incompatible.".
+// If the first and last characters are the same, return "Two's a pair.".
+
+function flipEndChars(str) {
+    
+}
