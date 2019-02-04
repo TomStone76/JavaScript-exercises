b158cd72215faf6af7ffc13d8d7d36b314c26123
YQS <41525397+TomStone76@users.noreply.github.com>
2019-02-04 13:22:25 -0500

    commit

--- missingNum.js
+++ missingNum.js
@@ -2,5 +2,10 @@
 // (excluding one number) and returns the missing number.
 
 function missingNums(arr) {
-    
+    const reducer = (accumulator, currentVal) => accumulator + currentVal
+    let sum = arr.reduce(reducer)
+    return 55 - sum
+
 }
+
+console.log(missingNums([1, 2, 3, 4, 6, 7, 8, 9, 10]))
