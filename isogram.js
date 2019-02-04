e2b7f22456defc10a48957edfa0032a04b66f3a5
YQS <41525397+TomStone76@users.noreply.github.com>
2019-02-04 17:58:11 -0500

    commit

--- isogram.js
+++ isogram.js
@@ -3,5 +3,13 @@
 // whether or not it's an "isogram".
 
 function isIsogram(str) {
+    let lower = str.toLowerCase()
+    let arr = lower.split('')
+    let formatted = [...new Set(arr)].join('')
     
+    if (lower === formatted) {
+        return true
+    } else {
+        return false
+    }
 }
