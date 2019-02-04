ba658374e6a9f545ff7e0a57846496586f4f2424
YQS <41525397+TomStone76@users.noreply.github.com>
2019-02-04 13:24:05 -0500

    commit

---
+++ museum.js
@@ -0,0 +1,10 @@
+// A museum wants to get rid of some exhibitions. Vanya, the interior architect, 
+// comes up with a plan to remove the most boring exhibitions. 
+// She gives them a rating, and removes the one with the lowest rating. 
+// Just as she finishes rating the exhibitions, she's called off to an important meeting. 
+// She asks you to write a program that tells her the ratings of the items after the lowest one is removed.
+// Create a function that takes an array of integers and removes the smallest value.
+
+function removeSmallest(arr) {
+    
+}
