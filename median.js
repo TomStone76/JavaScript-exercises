0c966c96e49419de88c529319f33ee63b4b9629a
YQS <41525397+TomStone76@users.noreply.github.com>
2019-02-06 13:24:38 -0500

    commit

--- median.js
+++ median.js
@@ -1,13 +1,17 @@
 // Create a function that takes an array of numbers and return its median.
-
 function median(arr) {
-    let sorted = arr.sort()
-    if (arr.length / 2 === 0) {
-        return (arr.length + 1) / 2
-    } else {
-        let first = arr.slice(0, arr.length/2)
-        return first
+    let s = arr.sort(function(a, b){return a - b})
+    if (s.length % 2 !== 0) {
+        return (s.length + 1) / 2
+    } else if (s.length % 2 === 0) {
+        for (let i = 0; i < s.length; i++) {
+            let first = s[s.length / 2]
+            let second = s[(s.length / 2) - 1]
+            return (first + second) / 2
+        } 
     }
 }
 
-console.log(median([2, 5, 6, 5, 6, 3, 4]))
+let x = [20, 40, 20, 30, 50, 60, 70, 0, 20]
+
+console.log(median(x))
