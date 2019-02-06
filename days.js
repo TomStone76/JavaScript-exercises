d64ae8e1b4d24bc6ba22c165d18df22e80d42627
YQS <41525397+TomStone76@users.noreply.github.com>
2019-02-06 14:29:47 -0500

    commit

--- days.js
+++ days.js
@@ -2,5 +2,25 @@
 // and returns the number of days in that month.
 
 function days(month, year) {
-    
+    let monthConv = month - 1
+    let first = new Date(year, monthConv)
+    let second = new Date(year, monthConv + 1)
+
+    if (year >= 1970) {
+        return Math.round((second - first) / (1000 * 60 * 60 * 24))
+    }
+    else if (year < 1970) {
+        return Math.round((second.setFullYear(year) - first.setFullYear(year)) / (1000 * 60 * 60 * 24))
+    }
 }
+console.log(days(2, 200))
+
+
+
+
+
+// two - one = difference in milliseconds
+//(two - one) / 1000 = difference in seconds
+// ((two - one) / 1000) / 60 = difference in minutes
+// ((two - one) / 1000) / 60) / 60 = difference in hours
+// (((two - one) / 1000) / 60) / 60) / 24 = days
