15d7e2b449cdb216b9fa69a8aaef491259dc2686
YQS <41525397+TomStone76@users.noreply.github.com>
2019-02-06 17:26:23 -0500

    commit

---
+++ letters.js
@@ -0,0 +1,6 @@
+// Create a function that accepts an array of two strings and checks if 
+// the letters in the second string are present in the first string.
+
+function letterCheck(arr) {
+    
+}
