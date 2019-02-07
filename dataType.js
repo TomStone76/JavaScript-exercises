eacfbf29d86048a23b3e947aef9101a289b60271
YQS <41525397+TomStone76@users.noreply.github.com>
2019-02-06 23:44:31 -0500

    commit

---
+++ dataType.js
@@ -0,0 +1,14 @@
+// Create a function that returns the data type of a given variable. 
+// These are the eight data types this challenge will be testing for:
+// Array
+// Object
+// String
+// Number
+// Boolean
+// Null
+// Undefined
+// Date
+
+function dataType(value) {
+    
+}
