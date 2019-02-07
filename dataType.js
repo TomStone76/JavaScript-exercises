e8fe072395c897055aaf51697c71e1a7e6b5fc4f
YQS <41525397+TomStone76@users.noreply.github.com>
2019-02-06 23:53:32 -0500

    commit

--- dataType.js
+++ dataType.js
@@ -1,14 +1,26 @@
 // Create a function that returns the data type of a given variable. 
 // These are the eight data types this challenge will be testing for:
 // Array
-// Object
-// String
-// Number
-// Boolean
-// Null
-// Undefined
+// Object typeof
+// String (typeof)
+// Number (typeof)
+// Boolean (typeof)
+// Null (typeof)
+// Undefined (typeof)
 // Date
 
 function dataType(value) {
-    
+    if () {
+        
+    }
 }
+
+
+
+// var x = [1, 2, 'f']
+
+// console.log(typeof x)
+
+// var y = new Date()
+
+// console.log(y instanceof Date)
