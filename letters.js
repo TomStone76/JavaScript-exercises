6e1dea4d346797e13ffdd374d862aaba4c73c7b5
YQS <41525397+TomStone76@users.noreply.github.com>
2019-02-06 20:44:14 -0500

    commit

--- letters.js
+++ letters.js
@@ -2,5 +2,24 @@
 // the letters in the second string are present in the first string.
 
 function letterCheck(arr) {
-    
+    let first = arr.shift().toLowerCase().split('');
+    let second = arr.pop().toLowerCase().split('');
+    let flag = true;
+    for (let i = 0; i < second.length; i++) {
+        if (!first.includes(second[i])) {
+            flag = false;
+        }
+    }
+    return flag;
 }
+
+console.log(letterCheck(["trances", "nectar"]))
+console.log(letterCheck(["THE EYES", "they see"]))
+console.log(letterCheck(["assert", "staring"]))
+console.log(letterCheck(["arches", "later"]))
+console.log(letterCheck(["dale", "caller"]))
+console.log(letterCheck(["parses", "parsecs"]))
+console.log(letterCheck(["replays", "adam"]))
+console.log(letterCheck(["mastering", "streaming"]))
+console.log(letterCheck(["drapes", "compadres"]))
+console.log(letterCheck(["deltas", "slated"]))
