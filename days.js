efb921c8d49e100d06e00235f8c84d61b4cfa0ae
YQS <41525397+TomStone76@users.noreply.github.com>
2019-02-08 23:40:40 -0500

    commit

--- days.js
+++ days.js
@@ -13,14 +13,3 @@ function days(month, year) {
         return Math.round((second.setFullYear(year) - first.setFullYear(year)) / (1000 * 60 * 60 * 24))
     }
 }
-console.log(days(2, 200))
-
-
-
-
-
-// two - one = difference in milliseconds
-//(two - one) / 1000 = difference in seconds
-// ((two - one) / 1000) / 60 = difference in minutes
-// ((two - one) / 1000) / 60) / 60 = difference in hours
-// (((two - one) / 1000) / 60) / 60) / 24 = days
