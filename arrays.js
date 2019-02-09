5893768cced5fbb93ba8b179dbe6ed26ba80c559
YQS <41525397+TomStone76@users.noreply.github.com>
2019-02-09 00:15:13 -0500

    commit

--- arrays.js
+++ arrays.js
@@ -1,26 +1,22 @@
 // Create a function that takes two arrays and combines them 
 // by alternatingly taking elements from each array in turn.
 
-let x = ['a', 'b', 'c']
-let y = [1, 2, 3, 4]
+let x = ['a', 'b', 'c', 'd']
+let y = [1, 2, 3, 4, 5, 6]
 
 function mergeArrays(a, b) {
     let flag = true
     let newArr = []
     let combined = a.concat(b)
-    if (flag === true) {
-        for (let i = 0; i < a.length; i++) {
-            newArr.push(a[i])
-            flag = false
-        }
-    }
-    if (flag === false) {
-        for (let i = 0; i < b.length; i++) {
+
+    for (let i = 0; i < a.length; i++) {
+        newArr.push(a[i])
+        for (let j = 0; j < 1; j++) {
             newArr.push(b[i])
-            flag = true
         }
     }
     return newArr
 }
 
 console.log(mergeArrays(x, y))
+
