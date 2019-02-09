3dde1b44c8080face9145185b6b2e2441dc850bd
YQS <41525397+TomStone76@users.noreply.github.com>
2019-02-08 23:55:20 -0500

    commit

--- arrays.js
+++ arrays.js
@@ -6,15 +6,21 @@ let y = [1, 2, 3, 4]
 
 function mergeArrays(a, b) {
     let flag = true
+    let newArr = []
     let combined = a.concat(b)
-    let formatted = []
     if (flag === true) {
         for (let i = 0; i < a.length; i++) {
-
-        }
-    } else if (flag === false) {
-        for (let i = 0; i < b.length; i++) {
-            
+            newArr.push(a[i])
+            flag = false
         }
     }
+    if (flag === false) {
+        for (let i = 0; i < b.length; i++) {
+            newArr.push(b[i])
+            flag = true
+        }
+    }
+    return newArr
 }
+
+console.log(mergeArrays(x, y))
