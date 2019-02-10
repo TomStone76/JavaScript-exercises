1caddb88f0c443fac23fedc236649e73a542bcb8
YQS <41525397+TomStone76@users.noreply.github.com>
2019-02-09 23:52:08 -0500

    commit

--- findOdd.js
+++ findOdd.js
@@ -1,8 +1,13 @@
 // Create a function that takes an array and 
 // finds the integer which appears an odd number of times.
 
+let a = [1, 1, 2, -2, 5, 2, 4, 4, -1, -2, 5]
+
 function findOdd(arr) {
-    for (let i = 0; i < arr.length; i++) {
-        
-    }
+    let modArr = arr.sort(function(a, b){return a - b});
+    let unique = Array.from(new Set(modArr));
+
+    return modArr, unique
 }
+
+console.log(findOdd(a))
