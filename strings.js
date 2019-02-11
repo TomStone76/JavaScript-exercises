e57dac74f4909fa531d8477d047916032dcd0f41
YQS <41525397+TomStone76@users.noreply.github.com>
2019-02-10 23:45:57 -0500

    commit

---
+++ strings.js
@@ -0,0 +1,13 @@
+// Create a function that, given a string with at least three characters, returns an array of its:
+
+// 1. Length.
+// 2. First character.
+// 3. Last character.
+// 4. Middle character, if the string has an odd number of characters. 
+// Middle TWO characters, if the string has an even number of characters.
+// 5. Index of the second occurrence of the second character in the format "@ index #" 
+// and "not found" if the second character doesn't occur again.
+
+function allAboutStrings(str) {
+    
+}
