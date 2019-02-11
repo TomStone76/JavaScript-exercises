9ad7a0f940d25522ebae683863db321d966ee570
YQS <41525397+TomStone76@users.noreply.github.com>
2019-02-10 23:12:23 -0500

    commit

--- removeSurrounding.js
+++ removeSurrounding.js
@@ -2,10 +2,6 @@
 // removes the surrounding duplicates and returns an array of items without 
 // any items with the same value next to each other and preserves the original order of items.
 
-
-let nums = [1, 2, 2, 3, 3, 3, 4, 4, 4, 4, 2, 2];
-let strings = ['a', 'b', 'b', 'c', 'c', 'c', 'd', 'd', 'd', 'd'];
-
 function uniqueInOrder(sequence) {
     let newArr = [];
 
@@ -16,5 +12,3 @@ function uniqueInOrder(sequence) {
     }
     return newArr;
 }
-
-console.log(uniqueInOrder(nums))
