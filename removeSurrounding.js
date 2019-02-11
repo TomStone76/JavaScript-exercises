0ef4b4bd340c2ace00f606f1769b3997d0839a35
YQS <41525397+TomStone76@users.noreply.github.com>
2019-02-10 22:38:49 -0500

    commit

---
+++ removeSurrounding.js
@@ -0,0 +1,7 @@
+// Create a function that takes a sequence of either strings or numbers, 
+// removes the surrounding duplicates and returns an array of items without 
+// any items with the same value next to each other and preserves the original order of items.
+
+function uniqueInOrder(sequence) {
+    
+}
