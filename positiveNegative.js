5335d66c35d7956443bd35244b627b51e9905aca
YQS <41525397+TomStone76@users.noreply.github.com>
2019-02-10 20:18:39 -0500

    commit

--- positiveNegative.js
+++ positiveNegative.js
@@ -2,6 +2,14 @@
 // Return an array where the first element is the count of positive numbers 
 // and the second element is the sum of negative numbers.
 
+let nums = [1, 2, 3, -1, -2, -3];
+
 function countPosSumNeg(arr) {
-    
+    let negative = [], positive = [];
+
+    for (let i = 0; i < arr.length; i++) {
+        if (arr[i] < 0) negative.push(arr[i]);
+        else if (arr[i] > 0) positive.push(arr[i]);
+    }
+
 }
