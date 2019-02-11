ebe86201ca5b1d6e4b75d6f2d07d1323297198dd
YQS <41525397+TomStone76@users.noreply.github.com>
2019-02-10 20:14:32 -0500

    commit

---
+++ positiveNegative.js
@@ -0,0 +1,7 @@
+// Create a function that takes an array of positive and negative numbers. 
+// Return an array where the first element is the count of positive numbers 
+// and the second element is the sum of negative numbers.
+
+function countPosSumNeg(arr) {
+    
+}
