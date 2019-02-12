4d97f4c26da7e465d0aff034d283063d7518ec9b
YQS <41525397+TomStone76@users.noreply.github.com>
2019-02-11 23:58:53 -0500

    commit

--- anagram.js
+++ anagram.js
@@ -1,5 +1,4 @@
-// Write a function that takes two strings and returns 
-// (true or false) whether they're anagrams or not.
+// Write a function that takes two strings and returns (true or false) whether they're anagrams or not.
 
 function isAnagram(s1, s2) {
     let mod1 = s1.toLowerCase().split('').sort().join('');
