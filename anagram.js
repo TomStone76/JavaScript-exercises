8fd1090adf3fba15250147069b8dcfcbae8f7fdb
YQS <41525397+TomStone76@users.noreply.github.com>
2019-02-11 23:47:20 -0500

    commit

--- anagram.js
+++ anagram.js
@@ -2,5 +2,8 @@
 // (true or false) whether they're anagrams or not.
 
 function isAnagram(s1, s2) {
+    let mod1 = s1.toLowerCase().split('').sort().join('');
+    let mod2 = s2.toLowerCase().split('').sort().join('');
 
+    return mod1 === mod2;
 }
