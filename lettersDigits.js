c2a9caab83e42524b6deb8416c909f70d5238eb7
YQS <41525397+TomStone76@users.noreply.github.com>
2019-02-12 23:25:58 -0500

    commit

--- lettersDigits.js
+++ lettersDigits.js
@@ -2,6 +2,26 @@
 // calculates the number of letters and digits within it. 
 // Return the result as an object.
 
+let x = "H3ll0 Wor1d";
+
 function countAll(str) {
-    
+    let count = {
+        "'LETTERS'": 0,
+        "'DIGITS'": 0
+    }
+
+    let letters = [], nums=[];
+
+    let formatted = str.split('').length
+
+    for (let i = 0; i < formatted; i++) {
+        if (typeof formatted[i] === 'string') {
+            letters.push(formatted)
+        }
+    }
+
 }
+
+
+
+console.log(count);
