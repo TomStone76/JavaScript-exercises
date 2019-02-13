b77f0d7aa6802ad132000a37c06a55b52a6c7f34
YQS <41525397+TomStone76@users.noreply.github.com>
2019-02-12 23:45:22 -0500

    commit

--- lettersDigits.js
+++ lettersDigits.js
@@ -7,22 +7,14 @@ let x = "H3ll0 Wor1d";
 function countAll(str) {
     let letters = [], numbers=[];
 
-    let formatted = str.split('');
-
-    for (let i = 0; i < formatted.length; i++) {
-        if (typeof formatted[i] === 'string') {
-            letters.push(formatted[i]);
-        } else if (typeof formatted[i] === 'number') {
-            numbers.push(formatted[i]);
-        }
-    }
+    const filtered = str.split('').filter(item => item != ' ');
 
     let count = {
         "LETTERS": letters.length,
         "DIGITS": numbers.length
     }
 
-   return formatted;
+   return count;
 }
 
 console.log(countAll(x));
