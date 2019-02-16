aa74b3cad2cbe1b558d463f590e67650bb4f1d3b
YQS <41525397+TomStone76@users.noreply.github.com>
2019-02-15 23:41:39 -0500

    commit

--- duplicateChars.js
+++ duplicateChars.js
@@ -1,19 +1,19 @@
 // Create a function that takes a string and returns the 
 // number of alphanumeric characters that occur more than once.
 
-function duplicateCount(str) {
-    let count = 0;
+// function duplicateCount(str) {
+//     let count = 0;
 
-    let sorted = str.toLowerCase().split('').sort();
+//     let sorted = str.toLowerCase().split('').sort();
 
-    for (let i = 0; i < sorted.length; i++) {
-        if (sorted[i] === sorted[i + 1]) {
-            count++;
-        }
-    }
+//     for (let i = 0; i < sorted.length; i++) {
+//         if (sorted[i] === sorted[i + 1]) {
+//             count++;
+//         }
+//     }
 
-    let set1 = new Set(sorted);
+//     let set1 = new Set(sorted);
 
-    let newArr = Array.from(set1).length;
+//     let newArr = Array.from(set1).length;
 
-console.log(duplicateCount('Indivisibilities'));
+// console.log(duplicateCount('Indivisibilities'));
