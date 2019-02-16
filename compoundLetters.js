0a00665ba554ea171e073e2b4a487b113411eb85
YQS <41525397+TomStone76@users.noreply.github.com>
2019-02-15 23:38:07 -0500

    commit

--- compoundLetters.js
+++ compoundLetters.js
@@ -2,6 +2,13 @@
 // new string with each new character accumulating by +1. 
 // Separate each set with a dash.
 
+let x = "abcs"
+
 function accum(str) {
-    
+    for (let i = 0; i < str.length; i++) {
+        let y = str[i].repeat(2)
+        return y
+    }
 }
+
+console.log(accum(x))
