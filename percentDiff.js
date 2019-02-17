aa1249ba75cbf4cc54ef1ea86f8c39e7227018cc
YQS <41525397+TomStone76@users.noreply.github.com>
2019-02-16 23:42:09 -0500

    commit

--- percentDiff.js
+++ percentDiff.js
@@ -1,6 +1,10 @@
 // Create a function that takes two numbers as arguments 
 // and return the percent difference between them.
 
+
 function percentDiff(num1, num2) {
-	
+    let absolute = Math.abs(num1 - num2);
+    let diff = absolute / ((num1 + num2) / 2);
+    return diff;
 }
+
