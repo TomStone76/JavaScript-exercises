dfd771f7aa3e2ecb68907bd296d1e4773f4ddd7b
YQS <41525397+TomStone76@users.noreply.github.com>
2019-02-17 23:58:46 -0500

    commit

--- keysVals.js
+++ keysVals.js
@@ -4,5 +4,7 @@
 var obj = ({a: 1, b: 2, c: 3});
 
 function keysAndValues(obj) {
-    return Object.keys(obj);
+    return Object.keys(obj) + Object.values(obj);
 }
+
+console.log(keysAndValues(obj))
