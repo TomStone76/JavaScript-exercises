b2449f4f7a40164754bc31724c441b9ee30ea786
YQS <41525397+TomStone76@users.noreply.github.com>
2019-02-17 23:36:17 -0500

    commit

--- firstLast.js
+++ firstLast.js
@@ -2,5 +2,8 @@
 // return the first and last elements as a new array.
 
 function firstLast(arr) {
-    
+    let newArr = [];
+    newArr.push(arr[0]);
+    newArr.push(arr[arr.length - 1]);
+    return newArr;
 }
