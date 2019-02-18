378485479f5a5c08b5a4543f100ac9fdf9f69bfc
YQS <41525397+TomStone76@users.noreply.github.com>
2019-02-17 23:56:28 -0500

    keyVals.js

---
+++ keysVals.js
@@ -0,0 +1,8 @@
+// Create a function that takes an object and 
+// returns the keys and values as separate arrays.
+
+var obj = ({a: 1, b: 2, c: 3});
+
+function keysAndValues(obj) {
+    return Object.keys(obj);
+}
