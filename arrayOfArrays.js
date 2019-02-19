6a3ca0b8e2a8791a8bd24fbfeb9b4acf104c96d1
YQS <41525397+TomStone76@users.noreply.github.com>
2019-02-18 23:58:23 -0500

    commit

---
+++ arrayOfArrays.js
@@ -0,0 +1,11 @@
+// Create a function that takes three arguments (x, y, z) and 
+// returns an array containing subarrays (e.g. [[], [], []]), 
+// each of a certain number of items, all set to either a string or an integer.
+
+// x argument: Number of subarrays contained within the main array.
+// y argument Number of items contained within each subarray(s).
+// z argument: Item contained within each subarray(s).
+
+function matrix(x, y, z) {
+    
+}
