7f3e10bae5e4f46dfbd4915d2e7f08a554ea01a3
YQS <41525397+TomStone76@users.noreply.github.com>
2019-02-20 23:58:00 -0500

    commit

--- minimum.js
+++ minimum.js
@@ -1,13 +1,6 @@
 // Write a function min that takes two arguments and returns their minimum.
 
-// console.log(min(0, 10));
-// → 0
-// console.log(min(0, -10));
-// → -10
-
 function min(x, y) {
     let z = Math.min(x, y);
     return z;
 }
-
-console.log(min(6.2, 6.1));
