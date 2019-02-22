acc3eef3644dea7956cb431cad905c1979818aab
YQS <41525397+TomStone76@users.noreply.github.com>
2019-02-21 22:43:21 -0500

    commit

---
+++ specChars.js
@@ -0,0 +1,7 @@
+// Create a function that takes a string, removes all "special" characters 
+// (e.g. ! @ # $ % ^ & \ *) and returns the new string. 
+// The only non-alphanumeric characters allowed are dashes -, underscores _ and spaces.
+
+function removeSpecialCharacters(str) {
+    
+}
