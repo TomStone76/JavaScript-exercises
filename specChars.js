24db2937df6b98c7f6d0a531a377bf67103b68a9
YQS <41525397+TomStone76@users.noreply.github.com>
2019-02-22 22:53:28 -0500

    commit

--- specChars.js
+++ specChars.js
@@ -3,7 +3,10 @@
 // The only non-alphanumeric characters allowed are dashes -, underscores _ and spaces.
 
 function removeSpecialCharacters(str) {
-    return str.replace(/\W|_/g, '');
+    // return str.replace(/\W|_/g, '');
+    for (let i = 0; i < str.length; i++) {
+        if (str[i] === '!' 
+    }
 }
 
 console.log(removeSpecialCharacters('%fd76$fd(-)6GvKlO.'));
