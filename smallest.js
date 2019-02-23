4eab342b532880d7ea3ca22f08bb300345d87fdd
YQS <41525397+TomStone76@users.noreply.github.com>
2019-02-22 23:41:33 -0500

    commit

--- smallest.js
+++ smallest.js
@@ -1,14 +1,6 @@
 // Create a function that takes an array of numbers
 // and returns the sum of the two lowest positive numbers.
 
-
-
-// let duplicates = sorted.filter(function(x, y) {
-//     return sorted.indexOf(x) >= y;
-// });
-
-var arr = [12, 435, 234, 123, 643, 658, 124]
-
 function sumTwoSmallestNums(arr) {
     let x = arr.sort(), y = []
     y.push(x.shift())
@@ -17,5 +9,3 @@ function sumTwoSmallestNums(arr) {
     let z = y.reduce(reducer)
     return z
 }
-
-console.log(sumTwoSmallestNums(arr))
