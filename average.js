f0e06a63d7872b979116e6bb02a207314a45d1fd
YQS <41525397+TomStone76@users.noreply.github.com>
2019-02-22 23:44:44 -0500

    commit

--- average.js
+++ average.js
@@ -1,9 +1,5 @@
-let x = [90, 87, 68, 55, 99, 85, 87, 82, 50, 97]
-
 function average(arr) {
     const reducer = (accumulator, currentVal) => accumulator + currentVal
     let result = arr.reduce(reducer) / arr.length
     return result
 }
-
-console.log(average(x))
