96bcc9858e4feba5b4c11e60f047cb697b4507e6
YQS <41525397+TomStone76@users.noreply.github.com>
2019-02-24 23:55:00 -0500

    commit

--- negative.js
+++ negative.js
@@ -2,5 +2,9 @@
 // returns true if the number is negative, false otherwise.
 
 function isNegative(value) {
-    
+    if (value < 0) {
+        return true;
+    } else if (value >= 0) {
+        return false;
+    }
 }
