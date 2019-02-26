2fdd6f289d161392598e7296b2ffdb46c85c30c9
YQS <41525397+TomStone76@users.noreply.github.com>
2019-02-25 21:41:06 -0500

    commit

---
+++ remainder.js
@@ -0,0 +1,8 @@
+// There is a single operator in Javascript capable of providing the remainder of a division operation. 
+// Two numbers are passed as parameters. 
+// The first provider divided by the second parameter will have a remainder, possiby zero. 
+// Return that value.
+
+function remainder(x, y) {
+    
+}
