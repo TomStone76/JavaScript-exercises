312587a36f79b07159ed8eaf1d7bf274e97b2627
YQS <41525397+TomStone76@users.noreply.github.com>
2019-02-25 23:59:06 -0500

    commit

--- capitalNames.js
+++ capitalNames.js
@@ -1,6 +1,8 @@
 // Create a function that takes an array of names and 
 // returns an array with the first letter capitalized.
 
+let names = ['steve', 'mIkE', 'jOHN'];
+
 function capMe(arr) {
     const cap = arr.map(x => x[0].toUpperCase());
     return cap.join(' ');
