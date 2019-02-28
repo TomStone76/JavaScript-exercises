fe8c96f4154bf7737b4c1b8bc80bb36f5f8a9296
YQS <41525397+TomStone76@users.noreply.github.com>
2019-02-27 23:50:11 -0500

    commit

--- lastChar.js
+++ lastChar.js
@@ -7,10 +7,13 @@ let three = "sample partner autonomy swallow trend";
 
 function sortByLast(str) {
     let arr = str.split(' '); // breaks string into array
-    const newArr = arr.map(function(item) {
+    const arr2 = arr.map(function(item) {
         return item.split('').reverse().join('');
     })
-    return newArr.sort();
+    const arr3 = arr2.map(function(item) {
+        return item.split('').reverse().join('');
+    })
+    return arr2.sort();
 }
 
 console.log(sortByLast(one));
