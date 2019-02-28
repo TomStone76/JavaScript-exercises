fdbf0b8bdcf8696259d0a94e220ee681a4f5f98e
YQS <41525397+TomStone76@users.noreply.github.com>
2019-02-27 23:02:54 -0500

    commit

---
+++ top.js
@@ -0,0 +1,7 @@
+// Create a function that takes an array of students and 
+// returns an array of their top notes. If student does 
+// not have notes then let's assume their top note is equal to 0.
+
+function getStudentTopNotes(students) {
+    
+}
