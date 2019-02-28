86ba73cce12f1e0ed9bf348262f9e67c8794ab56
YQS <41525397+TomStone76@users.noreply.github.com>
2019-02-27 23:26:05 -0500

    commit

---
+++ lastChar.js
@@ -0,0 +1,10 @@
+// Create a function that takes a string of words and 
+// return a string sorted alphabetically by the last character of each word.
+
+let one = "herb camera dynamic";
+let two = "stab traction artist approach";
+let three = "sample partner autonomy swallow trend";
+
+function sortByLast(str) {
+    let arr = str.split(' ');
+}
