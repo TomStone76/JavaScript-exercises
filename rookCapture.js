5760a68f26865385e138c9ce50eafee7ed4c6d9a
YQS <41525397+TomStone76@users.noreply.github.com>
2019-03-11 20:25:48 -0400

    commit

---
+++ rookCapture.js
@@ -0,0 +1,14 @@
+// A chessboard has rows numbered 1-8 and columns numbered A-H. 
+// In chess, rooks are pieces that can any number of squares horizontally or vertically.
+
+// Given the location of your rook and your opponent's rook, determine whether or not 
+// you can capture your opponent's rook with your own. For this exercise, assume there 
+// are no other pieces that are blocking. Your position and your opponent's position are 
+// represented as the first and second element of the input array, respectively.
+
+// For instance, in this example: canCapture(["A8", "E8"]) ➞ true your rook (at A8) 
+// can take your opponents rook (at E8) by moving horizontally.
+
+function canCapture([yourRook, opponentsRook]) {
+    
+}
