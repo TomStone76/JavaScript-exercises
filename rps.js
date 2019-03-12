1b44961252324aae4e8f7aab6bcdf171dd71ff8e
YQS <41525397+TomStone76@users.noreply.github.com>
2019-03-11 22:37:23 -0400

    commit

---
+++ rps.js
@@ -0,0 +1,16 @@
+// Abigail and Benson are playing Rock, Paper, Scissors.
+
+// Each game is represented by an array of length 2, where 
+// the first element represents what Abigail played and the 
+// second element represents what Benson played.
+
+// Given a sequence of games, determine who wins the most 
+// number of matches. If they tie, output "Tie".
+
+// R stands for Rock
+// P stands for Paper
+// S stands for Scissors
+
+function calculateScore(games) {
+    
+}
