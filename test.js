df108381e847073774800d27e4e902acbc63dac9
YQS <41525397+TomStone76@users.noreply.github.com>
2019-03-11 20:15:30 -0400

    commit

--- test.js
+++ test.js
@@ -1,8 +1,3 @@
-let num = Math.floor(Math.random() * 10)
-
-let param = process.argv[2]
-
-function compare(x) {
-    let y = (x === num)
-    return y ? "correct guess!" : "incorrect guess!"
-}
+x = 5;
+var x;
+console.log(x);
