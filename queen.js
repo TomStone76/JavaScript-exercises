5373bb2df36be133b3e26c2bcad9672ad6503c91
YQS <41525397+TomStone76@users.noreply.github.com>
2019-03-12 22:09:12 -0400

    commit

---
+++ queen.js
@@ -0,0 +1,9 @@
+// In chess, queens can move any number of squares horizontally, vertically or diagonally.
+
+// Given the location of your queen and your opponents' queen, determine whether or not you're 
+// able to capture your opponent's queen. Your location and your opponents' location are the 
+// first and second elements of the array, respectively.
+
+function canCapture([myQueen, opponentsQueen]) {
+    
+}
