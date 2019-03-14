82a98ea3f38333305694775bdcc47baae09b1082
YQS <41525397+TomStone76@users.noreply.github.com>
2019-03-13 21:45:56 -0400

    commit

--- queen.js
+++ queen.js
@@ -4,12 +4,14 @@
 // able to capture your opponent's queen. Your location and your opponents' location are the 
 // first and second elements of the array, respectively.
 
-// function canCapture([myQueen, opponentsQueen]) {
-//     let m = [], o = [], c = y + o;
-//     m.push(myQueen); o.push(opponentsQueen);
-//     if (c[0] === c[2] || c[1] === c[3]) {
-//         return true;
-//     } else if ()
-// }
+function canCapture([myQueen, opponentsQueen]) {
+    let m = [], o = [], c = y + o;
+    m.push(myQueen); o.push(opponentsQueen);
+    if (c[0] === c[2] || c[1] === c[3]) {
+        return true;
+    } else if () {
+        
+    }
+}
 
 console.log();
