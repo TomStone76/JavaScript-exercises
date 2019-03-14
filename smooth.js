12702387a85294b6ae8bee7038c3f9aa661f1c46
YQS <41525397+TomStone76@users.noreply.github.com>
2019-03-13 22:13:17 -0400

    commit

---
+++ smooth.js
@@ -0,0 +1,12 @@
+// Carlos is a huge fan of something he calls smooth sentences. 
+// A smooth sentence is one where the last letter of each word is identical to the first letter the following word.
+
+// For example, the following would be a smooth sentence:
+
+// "Carlos swam masterfully."
+
+// Since "Carlos" ends with an "s" and swam begins with an "s" and swam ends with an "m" and masterfully begins with an "m".
+
+function isSmooth(sentence) {
+    
+}
