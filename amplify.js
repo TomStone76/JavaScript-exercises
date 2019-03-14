7e86c3fc17583ac1781a8ecc87980f8318c3b388
YQS <41525397+TomStone76@users.noreply.github.com>
2019-03-13 21:47:54 -0400

    commit

---
+++ amplify.js
@@ -0,0 +1,8 @@
+// Create a function that takes an integer and returns an array from 1 to the given number, where:
+
+// If the number can be divided evenly by 4, amplify it by 10 (i.e. return 10 times the number).
+// If the number cannot be divided evenly by 4, simply return the number.
+
+function amplify(num) {
+    
+}
