086529f5a46c3ee4f62e94aebd0f95e523f13cd0
YQS <41525397+TomStone76@users.noreply.github.com>
2019-03-16 11:11:31 -0400

    commit

--- minimum.js
+++ minimum.js
@@ -1,6 +1,5 @@
 // Write a function min that takes two arguments and returns their minimum.
 
 function min(x, y) {
-    let z = Math.min(x, y);
-    return z;
+    return Math.min(x, y);
 }
--- nextNum.js
+++ nextNum.js
@@ -1,6 +1,5 @@
 // Create a function that takes a number as an argument, increments the number by +1 and returns the result.
 
 function addition(num) {
-    var x = num + 1;
-    return x
+    return num + 1;
 }
