04e5f614ac651c1b8f1116c6d0c4fb7fc30aa7c9
YQS <41525397+TomStone76@users.noreply.github.com>
2019-03-16 11:06:59 -0400

    commit

--- pi.js
+++ pi.js
@@ -1,6 +1,5 @@
 // Given a number n, write a function that returns PI to n decimal places.
 
 function myPi(n) {
-    var x = parseFloat(Math.PI.toFixed(n));
-    return x;
+    return parseFloat(Math.PI.toFixed(n));
 }
