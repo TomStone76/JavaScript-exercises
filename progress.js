068ead9425e6d1dfd269284c92606f9ece23e9c8
YQS <41525397+TomStone76@users.noreply.github.com>
2019-03-16 12:24:09 -0400

    progress.js setup

---
+++ progress.js
@@ -0,0 +1,10 @@
+// To train for an upcoming marathon, Johnny goes on one long-distance run each Saturday. 
+// He wants to track how often the number of miles he runs exceeds the previous Saturday. 
+// This is called a progress day.
+
+// Create a function that takes in an array of miles run every Saturday and returns Johnny's 
+// total number of progress days.
+
+function progressDays(runs) {
+    
+}
