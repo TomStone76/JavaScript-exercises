ad18d3a2bb7c47688330f48cf6255fa811b23998
YQS <41525397+TomStone76@users.noreply.github.com>
2019-03-24 15:08:24 -0400

    alliteration.js

---
+++ removeSpecial.js
@@ -0,0 +1,3 @@
+Create a function that takes a string, removes all "special" characters (e.g. ! @ # $ % ^ & \ *) and returns the new string. 
+The only non-alphanumeric characters allowed are dashes -, underscores _ and spaces.
+
