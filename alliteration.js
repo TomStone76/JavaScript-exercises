6d53e8fe44a7b200b9cef9da2c8bd9583dac6f53
YQS <41525397+TomStone76@users.noreply.github.com>
2019-03-24 15:09:39 -0400

    set up function

---
+++ alliteration.js
@@ -0,0 +1,7 @@
+// Alliteration refers to a sequence of words that begin with the same letter. 
+// For this exercise, a sentence is correctly alliterated if all words strictly 
+// greater than 3 characters begins with the same letter.
+
+function alliterationCorrect(sentence) {
+    
+}
