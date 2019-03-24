468c2906839ad8018729a02de089308aae03f676
YQS <41525397+TomStone76@users.noreply.github.com>
2019-03-24 15:42:13 -0400

    solution

--- alliteration.js
+++ alliteration.js
@@ -3,5 +3,8 @@
 // greater than 3 characters begins with the same letter.
 
 function alliterationCorrect(sentence) {
-    
+    const filtered = sentence.split(' ').filter(word => word.length > 3);
+    const first = filtered.map(x => x[0]);
+    let test = first.every((x, y, z) => x === z[0]);
+    return test;
 }
