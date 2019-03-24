f27f8b20baf17bbb9c711a1e187b08e578d7a53a
YQS <41525397+TomStone76@users.noreply.github.com>
2019-03-24 10:52:43 -0400

    loop

--- adverbs.js
+++ adverbs.js
@@ -2,5 +2,10 @@
 // An adverb is a word that ends with ly.
 
 function countAdverbs(sentence) {
-
+    let arr = sentence.substr(0, sentence.length - 1).split(' ');
+    for (let i = 0; i < arr.length; i++) {
+        
+    }
 }
+
+console.log(countAdverbs('She ran hurriedly towards the stadium.'))
