c883c6a421f2a70106b73d04ec3018bc6c779f51
YQS <41525397+TomStone76@users.noreply.github.com>
2019-03-24 00:24:55 -0400

    swap.js

--- syllables.js
+++ syllables.js
@@ -2,11 +2,11 @@
 // Each syllable is separated with a dash -.
 
 function numberSyllables(word) {
-    let numDash = 0;
-    for (let i = 0; i < word.length; i++) {
-        if (word[i] == "-") {numDash++};
+    let numDash=0;
+    for (let i=0; i<word.length; i++) {
+        if (word[i]=="-") {numDash++};
     }
-    return numDash + 1;
+    return numDash+1;
 }
 
 console.log(numberSyllables('on-o-mat-o-poe-ia'))
