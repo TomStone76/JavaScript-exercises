35088f7a2ecbb330ba49a1a796ad9d1dda4bb9d2
YQS <41525397+TomStone76@users.noreply.github.com>
2019-03-23 23:25:35 -0400

    solution

--- syllables.js
+++ syllables.js
@@ -2,5 +2,11 @@
 // Each syllable is separated with a dash -.
 
 function numberSyllables(word) {
-    
+    let numDash = 0;
+    for (let i = 0; i < word.length; i++) {
+        if (word[i] == "-") {numDash++};
+    }
+    return numDash + 1;
 }
+
+console.log(numberSyllables('on-o-mat-o-poe-ia'))
