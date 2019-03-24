0b404ec804de7823dc4c70f83b22abeb399d2fc3
YQS <41525397+TomStone76@users.noreply.github.com>
2019-03-24 12:59:52 -0400

    set up functions

---
+++ prefixSuffix.js
@@ -0,0 +1,14 @@
+// Create two functions: isPrefix(word, prefix-) and isSuffix(word, -suffix).
+
+// isPrefix should return true if it begins with the prefix argument.
+// isSuffix should return true if it ends with the suffix argument.
+
+// Otherwise return false.
+
+function isPrefix(word, prefix) {
+
+}
+
+function isSuffix(word, suffix) {
+    
+}
