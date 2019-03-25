c73dd24c1fd26f6bbbfd2bd6da786ef2fd26e7b2
YQS <41525397+TomStone76@users.noreply.github.com>
2019-03-25 19:04:10 -0400

    solution

---
+++ jackpot.js
@@ -0,0 +1,7 @@
+// Create a function that takes in an array (slot machine outcome) and 
+// returns true if all elements in the array are identical, and false otherwise. 
+// The array will contain 4 elements.
+
+function testJackpot(result) {
+    return result.every((a, b, c) => a ===c[0]);
+}
