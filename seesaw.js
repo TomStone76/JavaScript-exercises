c1b634eb360c6ff8a8d35b9fcb7c5d1e27904a27
YQS <41525397+TomStone76@users.noreply.github.com>
2019-03-25 13:14:51 -0400

    setup seesaw function

---
+++ seesaw.js
@@ -0,0 +1,11 @@
+// A number is left-heavy if the digits on the left side are larger than the digits on the right. 
+// It is right-heavy if the digits on the right side are larger than the digits on the left. 
+// Else, it is balanced.
+
+// Write a function that takes in an integer and classifies it into one of the three 
+// mutually exclusive categories: left, right or balanced. For inputs with an odd number 
+// of integers, ignore the fulcrum (the middle number).
+
+function seesaw(num) {
+    
+}
