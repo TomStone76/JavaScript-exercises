4e9f25c6ae2775af3572be4426622bcc58bdc649
YQS <41525397+TomStone76@users.noreply.github.com>
2019-03-26 21:24:32 -0400

    commit

---
+++ noYelling.js
@@ -0,0 +1,6 @@
+// Create a function that transforms sentences ending with multiple question marks ? 
+// or exclamation marks ! into a sentence only ending with one.
+
+function noYelling(phrase) {
+    
+}
