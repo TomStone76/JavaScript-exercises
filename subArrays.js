0a8e9e58a57b84ed6e07d32167a829f6ebe4f5e9
YQS <41525397+TomStone76@users.noreply.github.com>
2019-03-28 00:50:10 -0400

    partial solution

--- subArrays.js
+++ subArrays.js
@@ -2,5 +2,17 @@
 // returns the number of sub-arrays with identical elements.
 
 function countIdentical(arr) {
+    const check = arr.every((a) => a.length === 1);
+    const filtered = arr.filter(function(a) {
+        return a.every((x, y, z) => x === z[0]);
+    })
 
+    // if (check) {
+    //     return arr.length;
+    // } else if (!check) {
+    //     return filtered.length;
+    // }
+    
 }
+
+console.log(countIdentical([[1], [2, 3], [4], [5]]));
