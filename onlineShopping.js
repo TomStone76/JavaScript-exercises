a96e83bb2e8ab596ff8c04f67718fb67a86e43c9
YQS <41525397+TomStone76@users.noreply.github.com>
2019-03-30 16:26:01 -0400

    typingGame.js

--- onlineShopping.js
+++ onlineShopping.js
@@ -2,5 +2,14 @@
 // An order is eligible for free shipping if the total cost of items purchased exceeds $50.00.
 
 function freeShipping(order) {
-    
+    const reducer = (a, b) => a + b;
+    const mapped = order.map(val => val[])
 }
+
+const obj = {
+    "Monopoly": 11.99,
+    "Secret Hitler": 35.99,
+    "Bananagrams": 13.99
+}
+
+console.log(freeShipping(obj));
