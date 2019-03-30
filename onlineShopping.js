7e9c0c3b8c6d17e3ab9e780172275165419f1e55
YQS <41525397+TomStone76@users.noreply.github.com>
2019-03-30 15:06:37 -0400

    set up funciton

---
+++ onlineShopping.js
@@ -0,0 +1,6 @@
+// Create a function that determines whether a shopping order is eligible for free shipping.
+// An order is eligible for free shipping if the total cost of items purchased exceeds $50.00.
+
+function freeShipping(order) {
+    
+}
