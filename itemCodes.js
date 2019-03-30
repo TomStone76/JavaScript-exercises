3d21dd67e39aa5c5eb86b6351608a2cb5d77c34e
YQS <41525397+TomStone76@users.noreply.github.com>
2019-03-30 14:11:34 -0400

    set up funciton

---
+++ itemCodes.js
@@ -0,0 +1,7 @@
+// You have an array of item codes with the following format: "[letters][digits]"
+
+// Create a function that splits these strings into their alphabetic and numeric parts.
+
+function splitCode(item) {
+    
+}
