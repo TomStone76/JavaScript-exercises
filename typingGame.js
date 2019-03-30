3e728292aeaf958aafc72f3fa9befe3682d3f372
YQS <41525397+TomStone76@users.noreply.github.com>
2019-03-30 16:27:10 -0400

    set up function

---
+++ typingGame.js
@@ -0,0 +1,9 @@
+// You're in the midst of creating a typing game.
+
+// Create a function that takes in two arrays: the array of user-typed words, 
+// and the array of correctly-typed words and outputs an array containing 1s 
+// (correctly-typed words) and -1s (incorrectly-typed words).
+
+function correctStream(user, correct) {
+    
+}
