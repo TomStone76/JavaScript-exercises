7449566d11c206446db08992c17174ddea26aac9
YQS <41525397+TomStone76@users.noreply.github.com>
2019-03-30 14:46:28 -0400

    commit

--- tempCodeRunnerFile.js
+++
@@ -1,7 +0,0 @@
- // for (let i = 0; i < formatted.length; i++) {
-    //     if (typeof formatted[i] === 'string') {
-    //         chars.push(formatted[i]);
-    //     } else if (typeof formatted[i] === 'number') {
-    //         nums.push(formatted[i]);
-    //     }
-    // }
