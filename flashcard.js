d2c7cb0071b6b639dc7bcb6e01b0bc96eaca503a
YQS <41525397+TomStone76@users.noreply.github.com>
2019-03-31 22:26:04 -0400

    set up function

---
+++ flashcard.js
@@ -0,0 +1,7 @@
+// Create a function that outputs the results of a flashcard. 
+// A flashcard is an array of three elements: a number, an operator symbol, and another number. 
+// Return the mathematical result of that expression.
+
+function flash([num, op, num2]) {
+    
+}
