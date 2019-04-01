142bd6972a78021b74716258053cadfdf9f03bb1
YQS <41525397+TomStone76@users.noreply.github.com>
2019-03-31 22:44:31 -0400

    solution

--- flashcard.js
+++ flashcard.js
@@ -3,5 +3,17 @@
 // Return the mathematical result of that expression.
 
 function flash([num, op, num2]) {
-    
+    if (op === 'x') return num * num2;
+    if (op === '+') return num + num2;
+    if (op === '-') return num - num2;
+    if (op === '/') {
+        return num2 == 0 ? undefined : parseFloat((num / num2).toFixed(2));
+    }
 }
+
+let a = [3, 'x', 7];
+let b = [5, '+', 7];
+let c = [10, '-', 9];
+let d = [10, '/', 3];
+
+console.log(flash(d));
