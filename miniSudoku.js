af6b8375b0920973f8c68a261ee25e9af003614d
YQS <41525397+TomStone76@users.noreply.github.com>
2019-03-31 23:45:33 -0400

    partial solution

--- miniSudoku.js
+++ miniSudoku.js
@@ -5,5 +5,15 @@
 // are no duplicates, and no numbers outside this range.
 
 function isMiniSudoku(square) {
+    let key = ['1', '2', '3', '4','5', '6', '7', '8', '9'];
+    let one = [], two = [], three = [];
+    // let merged = test.concat(square);
+    // return merged;
+    // one.push(square[0]); two.push(square[1]); three.push(square[2]);
+    // return one
+
+    return square.toString().split('').sort()
     
 }
+
+console.log(isMiniSudoku([[1, 3, 2], [9, 7, 8], [4, 5, 6]]));
