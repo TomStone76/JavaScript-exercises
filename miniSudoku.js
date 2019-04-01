098639f20459d073741244f757a35958f80f59c4
YQS <41525397+TomStone76@users.noreply.github.com>
2019-03-31 23:22:53 -0400

    commit

---
+++ miniSudoku.js
@@ -0,0 +1,9 @@
+// For this task, you will be given a completed 3x3 square, 
+// in the form of a two-dimensional array. 
+// Create a function that checks to make sure this 3x3 square 
+// contains each number from 1-9 exactly once. Make sure there 
+// are no duplicates, and no numbers outside this range.
+
+function isMiniSudoku(square) {
+    
+}
