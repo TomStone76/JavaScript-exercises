de5ea4253d5ad40c38f3f20cb91415ddf34a48e0
YQS <41525397+TomStone76@users.noreply.github.com>
2019-03-31 23:08:36 -0400

    set up function

---
+++ convenience.js
@@ -0,0 +1,7 @@
+// Given a total due and an array representing the amount of change in your pocket, 
+// determine whether or not you are able to pay for the item. Change will always be 
+// represented in the following order: quarters, dimes, nickels, pennies.
+
+function amountDue(change, amountDue) {
+    
+}
