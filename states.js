b1b86523a617768ed32fbe3d0165dd93f0a757a1
YQS <41525397+TomStone76@users.noreply.github.com>
2019-04-01 23:37:39 -0400

    solution

--- states.js
+++ states.js
@@ -4,5 +4,6 @@
 // Full names full
 
 function filterStateNames(arr, type) {
-    
+    if (type === 'full') return arr.filter(full => full.length > 2);
+    if (type === 'abb') return arr.filter(abb => abb.length === 2);
 }
