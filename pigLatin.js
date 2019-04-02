1906a55723825c728d344a64560f4a1300539a7f
YQS <41525397+TomStone76@users.noreply.github.com>
2019-04-02 17:10:01 -0400

    set up function

---
+++ pigLatin.js
@@ -0,0 +1,7 @@
+// Move the first letter of each word to the end of the word.
+// Add "ay" to the end of the word.
+// Words starting with a vowel (A, E, I, O, U) simply have "WAY" appended to the end.
+
+function pigLatin(str) {
+    
+}
