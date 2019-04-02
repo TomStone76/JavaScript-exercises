112bfd80303b343df91f07d064e067845e743b54
YQS <41525397+TomStone76@users.noreply.github.com>
2019-04-02 17:24:21 -0400

    partial solution

--- pigLatin.js
+++ pigLatin.js
@@ -3,5 +3,17 @@
 // Words starting with a vowel (A, E, I, O, U) simply have "WAY" appended to the end.
 
 function pigLatin(str) {
-    
+    let formatted = str.substring(0, str.length - 1).toLowerCase().split(' ');
+
+    const mapped = formatted.map(function(word) {
+        let subArr = word.split('');
+        let move = word.slice(1, word.length) + word[0] + 'ay'; // everything BUT the first letter
+        return move
+    })
+
+    return mapped;
 }
+
+let test1 = "Cats are great pets.";
+
+console.log(pigLatin(test1));
