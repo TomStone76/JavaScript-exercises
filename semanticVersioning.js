85d6fb8b3f4e9169ba4de5b377f78e759c2849e5
YQS <41525397+TomStone76@users.noreply.github.com>
2019-04-01 23:55:20 -0400

    Commit

--- semanticVersioning.js
+++ semanticVersioning.js
@@ -2,13 +2,15 @@
 // in the semantic versioning specification.
 
 function retrieveMajor(semver) {
-
+    return semver[0];
 }
 
 function retrieveMinor(semver) {
-
+    return semver.length > 5
+    ? semver.substr(2, 2) : semver[2];
 }
 
 function retrievePatch(semver) {
-    
+    return semver.lengt > 5
+    ? semver.substr(6, 2) : semver[4];
 }
