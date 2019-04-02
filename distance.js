0216fd775a92474ca5ef48c387635a192e8e0e9c
YQS <41525397+TomStone76@users.noreply.github.com>
2019-04-02 14:05:15 -0400

    set up function

---
+++ distance.js
@@ -0,0 +1,9 @@
+// Create a function that takes a string of four numbers. 
+// These numbers represent two separat points on a graph 
+// known as the x-axis (horizontal axis) and y-axis (vertical axis). 
+// Each number corresponds as follows: "x1, y1, x2, y2". 
+// Calculate the distance between x and y.
+
+function shortestDistance(str) {
+
+}
