9feada00290dce57aa5dcb199056f903984deec0
YQS <41525397+TomStone76@users.noreply.github.com>
2019-04-01 23:34:04 -0400

    commit

---
+++ states.js
@@ -0,0 +1,8 @@
+// Create a function that filters out an array of state names into two categories based on the second parameter.
+
+// Abbreviations abb
+// Full names full
+
+function filterStateNames(arr, type) {
+    
+}
