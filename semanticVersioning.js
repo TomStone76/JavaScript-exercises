30f7b00036cd6ab86b208272669effdaaa84a9c7
YQS <41525397+TomStone76@users.noreply.github.com>
2019-04-01 23:45:15 -0400

    functions

---
+++ semanticVersioning.js
@@ -0,0 +1,14 @@
+// Write three separate functions, one to retrieve each element
+// in the semantic versioning specification.
+
+function retrieveMajor(semver) {
+
+}
+
+function retrieveMinor(semver) {
+
+}
+
+function retrievePatch(semver) {
+    
+}
