ab1411d513eb233d74b2017f4c0f42dd6c62417e
YQS <41525397+TomStone76@users.noreply.github.com>
2019-04-06 09:36:13 -0400

    set up function

---
+++ partiallyHiddenString.js
@@ -0,0 +1,8 @@
+// Create a function that takes a phrase and transforms each word using the following rules:
+
+// Keep first and last character the same.
+// Transform middle characters into a dash -.
+
+function partiallyHide(phrase) {
+    
+}
