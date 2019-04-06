05db4db4a0eac26f20e6e6b095806960dded4723
YQS <41525397+TomStone76@users.noreply.github.com>
2019-04-06 09:46:00 -0400

    partial solution

--- partiallyHiddenString.js
+++ partiallyHiddenString.js
@@ -4,5 +4,21 @@
 // Transform middle characters into a dash -.
 
 function partiallyHide(phrase) {
-    
+    let arr = [];
+    let f = phrase.split(' ');
+    const m = f.map(word => word.split(''));
+
+    for (let i = 0; i < m.length; i++) {
+        if (m[i].length <= 2) {
+            arr.push(m[i]);
+        } else if (m[i].length > 2) {
+            let first = m[i].charAt(0);
+            let last = m[i].charAt(m[i].length - 1);
+            if ()
+        }
+    }
 }
+
+let s = 'The small pup gnawed a hole in the sock';
+
+console.log(partiallyHide(s));
