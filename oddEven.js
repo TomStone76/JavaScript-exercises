4f7c964e93f44a4c6385ace5fa189c4e7d2b540a
YQS <41525397+TomStone76@users.noreply.github.com>
2019-04-09 14:37:02 -0400

    solution

--- oddEven.js
+++ oddEven.js
@@ -2,5 +2,8 @@
 // for each odd element and decrementing (-1) for each even number.
 
 function transform(arr) {
-    
+    for (let i = 0; i < arr.length; i++) {
+        (arr[i] % 2 !== 0) ? arr[i]++ : arr[i]--;
+    }
+    return arr;
 }
