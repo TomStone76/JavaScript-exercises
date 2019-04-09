9978311bf09b6bac6632190321a21230e3ad9f99
YQS <41525397+TomStone76@users.noreply.github.com>
2019-04-08 23:16:12 -0400

    solution

--- hashesPluses.js
+++ hashesPluses.js
@@ -1,5 +1,11 @@
 // Create a function that returns the number of hashes and pluses in a string.
 
 function hashPlusCount(str) {
-    
+    let hash = 0, plus = 0, arr = [];
+    for (let i = 0; i < str.length; i++) {
+        if (str[i] === '#') hash++;
+        if (str[i] === '+') plus++; 
+    }
+    arr.push(hash); arr.push(plus);
+    return arr;    
 }
