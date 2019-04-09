112bab136e28ae99f5014da43310360527480f15
YQS <41525397+TomStone76@users.noreply.github.com>
2019-04-08 23:02:27 -0400

    commit

---
+++ sameCase.js
@@ -0,0 +1,6 @@
+// Create a function that returns true if an input string 
+// contains only uppercase or only lowercase letters.
+
+function sameCase(str) {
+    return (str === str.toUpperCase() || str === str.toLowerCase());
+}
