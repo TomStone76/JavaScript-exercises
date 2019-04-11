07afc3f009c5ba41205dab7ebdea67ec7089a5a1
YQS <41525397+TomStone76@users.noreply.github.com>
2019-04-11 18:41:14 -0400

    blackjack.js

---
+++ blackjack.js
@@ -0,0 +1,8 @@
+// Create a function that takes an array of card numbers
+//  and checks if the sum of their value exceeds 21. 
+//  If the sum exceeds 21, return true and if the sum is 
+//  under or equal to 21, return false.
+
+function overTwentyOne(cards) {
+    
+}
