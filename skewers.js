e314f5b5fb57c8e88988a15066fcee07e33e9604
YQS <41525397+TomStone76@users.noreply.github.com>
2019-04-13 17:00:01 -0400

    solution

--- skewers.js
+++ skewers.js
@@ -1,3 +1,10 @@
-function bbqSkewers() {
-    
+function bbqSkewers(grill) {
+    let nonVeg = [], veg =[], res = [];
+    for (let i = 0; i < grill.length; i++) {
+        grill[i].includes('x') ? 
+        nonVeg.push(grill[i])
+        : veg.push(grill[i]);
+    }
+    res.push(veg.length); res.push(nonVeg.length);
+    return res;
 }
