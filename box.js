85b4e31644921d0510c150d2a0cbeac772832b77
YQS <41525397+TomStone76@users.noreply.github.com>
2019-04-13 22:11:10 -0400

    solution

--- box.js
+++ box.js
@@ -1,5 +1,9 @@
 // Create a function that returns true if an asterisk * is inside a box.
 
 function inBox(arr) {
-    
+    flag = false;
+    for (let i = 0; i < arr.length; i++) {
+        if (arr[i].includes('*')) flag = true
+    }
+    return flag;
 }
