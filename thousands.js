7ab244d672faa162da77eae6740015ce48e425b8
YQS <41525397+TomStone76@users.noreply.github.com>
2019-04-18 21:17:27 -0400

    Commit

--- thousands.js
+++ thousands.js
@@ -2,5 +2,9 @@
 // returns a string formatted to separate thousands.
 
 function formatNumber(num) {
-    
+    if (num < 1000) {
+        return num;
+    }  else if (num >= 1000) {
+        let arr = num.split('');
+    }
 }
