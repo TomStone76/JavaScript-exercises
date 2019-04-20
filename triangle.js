6815007eb2b3133d2dd0a67c5d32eff8697a9908
YQS <41525397+TomStone76@users.noreply.github.com>
2019-04-19 22:31:04 -0400

    Commit

--- triangle.js
+++ triangle.js
@@ -1,13 +1,5 @@
 // Write a loop that makes seven calls to console.log to output the following triangle:
 
-// #
-// ##
-// ###
-// ####
-// #####
-// ######
-// #######
-
 for (let line = "#"; line.length < 8; line += "#") {
     return line
 }
