bc110836e4fa00a60d1a33d33efcd40eedaa9eac
YQS <41525397+TomStone76@users.noreply.github.com>
2019-04-19 22:26:15 -0400

    refactor

--- days.js
+++ days.js
@@ -6,20 +6,7 @@ function days(month, year) {
     let first = new Date(year, monthConv)
     let second = new Date(year, monthConv + 1)
 
-    if (year >= 1970) {
-        return Math.round((second - first) / (1000 * 60 * 60 * 24))
-    }
-    else if (year < 1970) {
-        return Math.round((second.setFullYear(year) - first.setFullYear(year)) / (1000 * 60 * 60 * 24))
-    }
+    return year >= 1970 ?
+    Math.round((second - first) / (1000*60*60*24))
+    : Math.round((second.setFullYear(year) - first.setFullYear(year)) / (1000*60*60*24))
 }
-
-// function days(month, year) {
-//     let monthConv = month - 1
-//     let first = new Date(year, monthConv)
-//     let second = new Date(year, monthConv + 1)
-
-//     return year >= 1970 ?
-//     Math.round((second - first) / (1000*60*60*24))
-//     : Math.round((second.setFullYear(year) - first.setFullYear(year)) / (1000*60*60*24))
-// }
