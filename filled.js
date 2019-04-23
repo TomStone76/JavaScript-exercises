bb85222424b865b3a34593db8d7dd40b6bb0a629
YQS <41525397+TomStone76@users.noreply.github.com>
2019-04-22 22:39:21 -0400

    solution

--- filled.js
+++ filled.js
@@ -1,3 +1,12 @@
 function completelyFilled(arr) {
-    
+    let flag = true;
+    for (let i = 0; i < arr.length; i++) {
+        if (arr[i].includes(' ')) flag = false;
+    }
+    return flag;
 }
+
+
+// function completelyFilled(arr) {
+//     return arr.join('').includes(' ');
+// }
