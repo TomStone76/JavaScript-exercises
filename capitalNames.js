1289d7233df033d30db3dc9a8f71ee8c97ccff95
YQS <41525397+TomStone76@users.noreply.github.com>
2019-04-30 21:44:54 -0400

    clean code

--- capitalNames.js
+++ capitalNames.js
@@ -1,12 +1,8 @@
 // Create a function that takes an array of names and 
 // returns an array with the first letter capitalized.
 
-let names = ['steve', 'mIkE', 'jOHN'];
-
 function capMe(arr) {
     const cap = arr.map(x => x[0].toUpperCase() + 
     x.substring(1, x.length).toLowerCase());
     return cap;
 }
-
-console.log(capMe(names));
