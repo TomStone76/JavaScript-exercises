caeee789b982a8df068137d6fbd5f53c8748c737
YQS <41525397+TomStone76@users.noreply.github.com>
2019-05-06 23:03:11 -0400

    commit

--- startVowel.js
+++ startVowel.js
@@ -1,3 +1,10 @@
 function retrieve(str) {
-    
+    let formatted = str.substr(0, str.length-1).split(' ');
+    let arr = [];
+    for (let i = 0; i < formatted.length; i++) {
+        if (formatted[i][0] === formatted.match(/[aeiou]/)) arr.push(formatted[i]);
+    }
+
+    return arr
 }
+console.log(retrieve("A simple life is a happy life for me."))
